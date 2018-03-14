d91a63775ece4f13faf0feb34a638730c34f7606
Marcin <marcin531@gmail.com>
2018-03-14 18:56:04 +0100

    Initial commit

---
+++ untitled/src/acca.java
@@ -0,0 +1,11 @@
+public class acca {
+}
+    int plus(int a, int b);
+    int plus(int a, int b)
+    {
+        int i;
+        int temp = a;
+        for (i = 1; i<b; i++)
+            a += temp;
+        return a;
+
